c90f685500ab41f43ecfdcdf86cfd7f5ba638317
Filip Sjölander <filip@sanalabs.com>
2024-09-05 00:00:27 +0200

    update design

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,62 @@ import styled from "styled-components";
 
 import { collection, addDoc, getDocs, Timestamp } from "firebase/firestore";
 import { db } from "./firestoreConfig";
+import image from "./nomio-runner.png";
 
-const Heading = styled.h1``;
+const Heading = styled.h1`
+  font-size: 5em;
+`;
+
+const Fade = styled.div`
+  position: absolute;
+  width: 400px;
+  height: 100%;
+  right: -400px;
+  background: linear-gradient(
+    to right,
+    rgba(2, 15, 15, 1) 35%,
+    74%,
+    rgba(1, 18, 15, 0)
+  );
+`;
+
+const ColoredBackground = styled.div`
+  position: relative;
+  background: rgba(2, 15, 15, 1);
+  display: flex;
+  flex-grow: 1;
+`;
+
+const BackgroundContainer = styled.div`
+  position: absolute;
+  inset: 0;
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  width: fit-content;
+  user-select: none;
+`;
+
+const Page = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+
+  color: white;
+  background: rgb(1, 18, 15);
+`;
 
 const SignUpButton = styled.button`
-  border: solid 1px white;
-  border-radius: 8px;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  border: transparent;
   padding: 8px;
-  color: white;
+  color: #ffffffc9;
   background-color: transparent;
   cursor: pointer;
   &:hover {
@@ -19,39 +67,82 @@ const SignUpButton = styled.button`
   }
 `;
 
+const StyledInput = styled.input`
+  border-bottom-left-radius: 8px;
+  border-top-left-radius: 8px;
+  border: transparent;
+  padding: 8px;
+  color: #ffffff;
+  background-color: transparent;
+`;
+
+const InputContainer = styled.div`
+  width: fit-content;
+  border: solid 1px white;
+  border-radius: 8px;
+  color: white;
+  background-color: transparent;
+
+  display: flex;
+  flex-direction: row;
+`;
+
 function App() {
   console.log("hello", { db });
   const [email, setEmail] = useState("");
+  const [hasSent, setHasSent] = useState(false);
   const addEmail = async (email: string) => {
     try {
       const docRef = await addDoc(collection(db, "emails"), {
         email: email,
         addedAt: Timestamp.now(),
       });
-      console.log("Document written with ID: ", docRef.id);
+      setHasSent(true);
+      setEmail("");
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <Heading>nomio</Heading>
-        <p>the website will launch soon...</p>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} />
-        <SignUpButton
-          onClick={async () => {
-            await addEmail(email);
-            console.log("trying to read");
-            const querySnapshot = await getDocs(collection(db, "emails"));
-            querySnapshot.forEach((doc) => {
-              console.log(`${doc.id} => ${doc.data()}`);
-            });
-          }}
-        >
-          Sign up here
-        </SignUpButton>
-      </header>
+      <Page>
+        <div style={{ zIndex: 1, margin: "120px", maxWidth: "50ch" }}>
+          <Heading>Nomio</Heading>
+          <p>We are coming soon.</p>
+          <p>
+            Get real with clinically tested isothiocyanates from 🥦 <br />
+            Reducing lactic acid by 12% and increases by endurance 8% on
+            average. Promotes quicker recovery for your volume training.
+          </p>
+          <p>and it works</p>
+          <InputContainer>
+            <StyledInput
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder={hasSent ? "Thank you!" : "Your email"}
+            />
+            <SignUpButton
+              onClick={async () => {
+                await addEmail(email);
+                console.log("trying to read");
+                const querySnapshot = await getDocs(collection(db, "emails"));
+                querySnapshot.forEach((doc) => {
+                  console.log(`${doc.id} => ${doc.data()}`);
+                });
+              }}
+            >
+              Sign up for early access to no6 stamina
+            </SignUpButton>
+          </InputContainer>
+          <p>Dig into the science here.</p>
+        </div>
+        <BackgroundContainer>
+          <Image src={image} />
+          <ColoredBackground>
+            <Fade />
+          </ColoredBackground>
+        </BackgroundContainer>
+      </Page>
     </div>
   );
 }
