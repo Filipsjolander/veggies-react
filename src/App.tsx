b8f3f3ddd71652a0e8932297ff8fc6e4262af4b4
Filip Sjölander <filip@sanalabs.com>
2024-10-24 17:00:34 +0200

    Remove background image

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 
 import { collection, addDoc, getDocs, Timestamp } from "firebase/firestore";
 import { db } from "./firestoreConfig";
-import image from "./nomio-runner.png";
 
 const Heading = styled.h1`
   font-size: 5em;
@@ -14,41 +13,42 @@ const Heading = styled.h1`
   @media (max-width: 600px) {
     font-size: 2em;
     margin-bottom: 1.5rem;
+    max-width: 10ch;
   }
 `;
 
-const Fade = styled.div`
-  position: absolute;
-  width: 400px;
-  height: 100%;
-  right: -400px;
-  background: linear-gradient(
-    to right,
-    rgba(2, 15, 15, 1) 35%,
-    74%,
-    rgba(1, 18, 15, 0)
-  );
-`;
+// const Fade = styled.div`
+//   position: absolute;
+//   width: 400px;
+//   height: 100%;
+//   right: -400px;
+//   background: linear-gradient(
+//     to right,
+//     rgba(2, 15, 15, 1) 35%,
+//     74%,
+//     rgba(1, 18, 15, 0)
+//   );
+// `;
 
-const ColoredBackground = styled.div`
-  position: relative;
-  background: rgba(2, 15, 15, 1);
-  display: flex;
-  flex-grow: 1;
-`;
+// const ColoredBackground = styled.div`
+//   position: relative;
+//   background: rgba(2, 15, 15, 1);
+//   display: flex;
+//   flex-grow: 1;
+// `;
 
-const BackgroundContainer = styled.div`
-  position: absolute;
-  inset: 0;
-  display: flex;
-  flex-direction: row-reverse;
-`;
+// const BackgroundContainer = styled.div`
+//   position: absolute;
+//   inset: 0;
+//   display: flex;
+//   flex-direction: row-reverse;
+// `;
 
-const Image = styled.img`
-  height: 100%;
-  width: fit-content;
-  user-select: none;
-`;
+// const Image = styled.img`
+//   height: 100%;
+//   width: fit-content;
+//   user-select: none;
+// `;
 
 const Page = styled.div`
   position: relative;
@@ -58,7 +58,7 @@ const Page = styled.div`
   flex-direction: column;
 
   color: white;
-  background: rgb(1, 18, 15);
+  background: rgb(0, 0, 0);
 `;
 
 const SignUpButton = styled.button`
@@ -89,7 +89,7 @@ const InputContainer = styled.div`
   border: solid 1px white;
   border-radius: 8px;
   color: white;
-  background-color: transparent;
+  background-color: rgba(1, 18, 15, 0.5);
 
   display: flex;
   flex-direction: row;
@@ -162,15 +162,12 @@ function App() {
             <Heading>Launching soon.</Heading>
 
             <p>
-              Get scientific with clinically tested isothiocyanates from{" "}
-              <span role="img" aria-label="Broccoli">
-                🥦
-              </span>
-              . Reduces lactic acid by 12% and increases endurance by 8% on
-              average during exercise. Promotes quicker recovery of muscles
-              post-exercise, for your volume training.
+              Improving athletic performance based on clinical science on
+              broccoli sprouts from researchers at GIH and KI. Results include
+              an average reduction of lactate during intense exercise by 12%,
+              and improved recovery.
             </p>
-            <p>And it works.</p>
+            <p>Stay updated by signing up:</p>
             <InputContainer>
               <StyledInput
                 value={email}
@@ -197,12 +194,6 @@ function App() {
             </LinkContainer>
           </Content>
         </ScrollContainer>
-        <BackgroundContainer>
-          <Image src={image} />
-          <ColoredBackground>
-            <Fade />
-          </ColoredBackground>
-        </BackgroundContainer>
       </Page>
     </div>
   );
